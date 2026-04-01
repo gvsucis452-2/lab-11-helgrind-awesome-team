2def90eda59deea2698bf7b061d629e53ac9a358
Reegan Graham <grahamree63@gmail.com>
2026-04-01 11:44:49 -0400

    fixing 2 locks

--- main-race.c
+++ main-race.c
@@ -4,7 +4,6 @@
 
 int balance = 0;
 pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t m2 = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* arg) {
     Pthread_mutex_lock(&m1);
@@ -17,9 +16,10 @@ int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
 
-    Pthread_mutex_lock(&m2);
+    Pthread_mutex_lock(&m1);
     balance++; // unprotected access
-    Pthread_mutex_unlock(&m2);
+    Pthread_mutex_unlock(&m1);
+    
     Pthread_join(p, NULL);
     return 0;
 }
